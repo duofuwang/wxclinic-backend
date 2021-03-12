0c820b661809c61cc56b83746b8afeb36c042c4b
duofuwang <duofu.wang@foxmail.com>
2021-03-13 04:35:57 +0800

    update: appid、secret改为静态注入，移除无用方法

--- src/main/java/com/dopoiv/clinic/common/utils/WechatUtil.java
+++ src/main/java/com/dopoiv/clinic/common/utils/WechatUtil.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.dopoiv.clinic.common.client.MyHttpClient;
 import org.apache.shiro.codec.Base64;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -13,10 +12,8 @@ import javax.annotation.PostConstruct;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.AlgorithmParameters;
-import java.security.Security;
+import java.nio.charset.StandardCharsets;
 import java.security.spec.AlgorithmParameterSpec;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,25 +25,28 @@ import java.util.Map;
 public class WechatUtil {
 
     private static MyHttpClient httpClient;
+    private static String appid;
+    private static String secret;
 
     @Autowired
     private MyHttpClient myHttpClient;
 
+    @Value("${wx.mini.appid}")
+    private String appidTemp;
+
+    @Value("${wx.mini.secret}")
+    private String secretTemp;
+
     @PostConstruct
     public void init() {
         httpClient = myHttpClient;
+        appid = appidTemp;
+        secret = secretTemp;
     }
-
-    @Value("${wx.mini.appid}")
-    private static String appid;
-
-    @Value("${wx.mini.secret}")
-    private static String secret;
 
     public static JSONObject getSessionKeyOrOpenId(String code) {
         String requestUrl = "https://api.weixin.qq.com/sns/jscode2session";
         Map<String, String> requestUrlParam = new HashMap<>();
-        // https://mp.weixin.qq.com/wxopen/devprofile?action=get_profile&token=164113089&lang=zh_CN
         //小程序appId
         requestUrlParam.put("appid", appid);
         //小程序secret
@@ -56,44 +56,7 @@ public class WechatUtil {
         //默认参数
         requestUrlParam.put("grant_type", "authorization_code");
         //发送post请求读取调用微信接口获取openid用户唯一标识
-        JSONObject jsonObject = JSON.parseObject(httpClient.doGet(requestUrl, requestUrlParam));
-        return jsonObject;
-    }
-
-    public static JSONObject getUserInfo(String encryptedData, String sessionKey, String iv) {
-        // 被加密的数据
-        byte[] dataByte = Base64.decode(encryptedData);
-        // 加密秘钥
-        byte[] keyByte = Base64.decode(sessionKey);
-        // 偏移量
-        byte[] ivByte = Base64.decode(iv);
-        try {
-            // 如果密钥不足16位，那么就补足.  这个if 中的内容很重要
-            int base = 16;
-            if (keyByte.length % base != 0) {
-                int groups = keyByte.length / base + (keyByte.length % base != 0 ? 1 : 0);
-                byte[] temp = new byte[groups * base];
-                Arrays.fill(temp, (byte) 0);
-                System.arraycopy(keyByte, 0, temp, 0, keyByte.length);
-                keyByte = temp;
-            }
-            // 初始化
-            Security.addProvider(new BouncyCastleProvider());
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
-            SecretKeySpec spec = new SecretKeySpec(keyByte, "AES");
-            AlgorithmParameters parameters = AlgorithmParameters.getInstance("AES");
-            parameters.init(new IvParameterSpec(ivByte));
-            // 初始化
-            cipher.init(Cipher.DECRYPT_MODE, spec, parameters);
-            byte[] resultByte = cipher.doFinal(dataByte);
-            if (null != resultByte && resultByte.length > 0) {
-                String result = new String(resultByte, "UTF-8");
-                return JSON.parseObject(result);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+        return JSON.parseObject(httpClient.doGet(requestUrl, requestUrlParam));
     }
 
     /**
@@ -116,7 +79,7 @@ public class WechatUtil {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             SecretKeySpec keySpec = new SecretKeySpec(keyByte, "AES");
             cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
-            return new String(cipher.doFinal(dataByte), "UTF-8");
+            return new String(cipher.doFinal(dataByte), StandardCharsets.UTF_8);
         } catch (Exception e) {
             e.printStackTrace();
         }
